596b3f70045b3534722554b1153abca73e5ee03f
Lotfi IKHADALLEM <dl_ikhadallem@esi.dz>
2018-03-11 21:58:35 +0000

    Replace login.component.spec.ts

--- frontend_web/src/log/login/login.component.spec.ts
+++ frontend_web/src/log/login/login.component.spec.ts
@@ -1,25 +1,46 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
-import { LoginComponent } from './login.component';
+import {Service} from './login.service';
 
-describe('LoginComponent', () => {
-  let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
+import { HttpHeaders, HttpClient , HttpClientModule } from '@angular/common/http';
 
-  beforeEach(async(() => {
+fdescribe ('Service ', () =>
+{
+  let httpMock : HttpTestingController;
+  let service : Service;
+
+  const endpoint = "https://auththarwa.cleverapps.io/oauth/code";
+  
+  const response = { succe: "l'utilisateur est verifie"};
+
+  beforeEach(() =>
+  {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
-  }));
+      imports : [HttpClientTestingModule],
+      providers : [Service]
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(LoginComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    service = TestBed.get(Service);
+    httpMock = TestBed.get(HttpTestingController);
+  });
+  
+  it('devrait envoyer une requete d\'authentification d\' gestionnaire',()=>
+  {
+
+    service.authentifier().subscribe(
+    data=> {
+      console.log(data);
+    });
+
+    const requete = httpMock.expectOne(endpoint);
+    httpMock.verify();
+    expect(requete.request.method).toBe('POST');
+    expect(requete.request.body).toBe("userId=test_tharwa@mailinator.com&Pwd=orca@2018&code=0");
+    expect(response).toBeTruthy();
+    requete.flush(response);
+
+
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
